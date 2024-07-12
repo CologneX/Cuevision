8dd39e90460bdbc47798c913934feb6447454f97
Kyrell <kleanosiauw@student.ciputra.ac.id>
2024-07-12 14:38:47 +0700

    init

---
+++ billihelper/billihelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  billihelperApp.swift
+//  billihelper
+//
+//  Created by Kyrell Leano Siauw on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct billihelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
