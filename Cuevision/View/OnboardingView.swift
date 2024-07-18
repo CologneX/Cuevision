ef43fca56acea4874cd1896c2c1e404142ba855b
gabrielabernice <gabrielabernice03@gmail.com>
2024-07-18 14:48:15 +0700

    finishing onboarding view

--- Cuevision/View/OnboardingView.swift
+++ Cuevision/View/OnboardingView.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var currentPage = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack {
+                Image("InfoBG")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    TabView(selection: $currentPage) {
+                        VStack {
+                            Image("ball3")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                            
+                            Text("Focus, Aim, Win")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding()
+                            
+                            Text("Improve your aim using diamond system scanner")
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                        }
+                        .padding()
+                        .tag(0)
+                        
+                        VStack {
+                            Image("OnboardingInfo")
+                                .padding(.leading, 50)
+                            
+                            NavigationLink(destination: HomeView()) {
+                                Text("Get Started")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .padding(.horizontal, 70)
+                                    .padding(.vertical, 15)
+                                    .background(Color.white)
+                                    .foregroundColor(Color(hex: 0x428365))
+                                    .cornerRadius(30)
+                            }
+                        }
+                        .padding()
+                        .tag(1)
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 40, height: 20)
+                            .cornerRadius(20)
+                            .foregroundColor(Color(hex: 0x89A597))
+                        
+                        HStack {
+                            ForEach(0..<2) { index in
+                                Circle()
+                                    .fill(index == currentPage ? Color.black : Color(hex: 0x617469))
+                                    .frame(width: 8, height: 8)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
