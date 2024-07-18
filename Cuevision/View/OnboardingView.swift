776ee844e97d5a24df061d3355963ea664b5997e
gabrielabernice <gabrielabernice03@gmail.com>
2024-07-18 14:47:52 +0700

    making onboarding view

---
+++ Cuevision/View/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Cuevision
+//
+//  Created by Gabriela Bernice Handoko on 18/07/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
