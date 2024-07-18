ee1fd3998193f8aca878fb16e9042e104be40b0c
Kyrell <kleanosiauw@student.ciputra.ac.id>
2024-07-19 00:37:54 +0700

    chore(Cuevision): change filenames and comments

--- Cuevision/Cuevision.swift
+++ Cuevision/Cuevision.swift
@@ -1,14 +1,14 @@
 //
-//  SwiftCameraApp.swift
-//  SwiftCamera
+//  CameraPreview.swift
+//  Cuevision
 //
-//  Created by Rolando Rodriguez on 10/15/20.
+//  Created by Kyrell Leano Siauw on 18/07/24.
 //
 
 import SwiftUI
 
 @main
-struct SwiftCameraApp: App {
+struct CuevisionApp: App {
     var body: some Scene {
         WindowGroup {
             CameraView()
