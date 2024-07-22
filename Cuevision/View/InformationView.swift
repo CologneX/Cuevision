aa32293d31f652ee74d09a764a090f955dd3df75
Fachmi Dimas Ardhana <fachmi2007@gmail.com>
2024-07-23 00:50:33 +0700

    refinement UI feature Billiard Tips

--- Cuevision/View/InformationView.swift
+++ Cuevision/View/InformationView.swift
@@ -8,52 +8,74 @@
 import SwiftUI
 
 struct InformationView: View {
+    
     @Environment(\.presentationMode) var presentationMode
     
+    struct BilliardTips: Hashable {
+        var image: String
+        var title: String
+        var subtitle: String
+    }
+    
+    let dataBilliardTips = [
+        BilliardTips(image: "solid_one", title: "Cue Ball Effect", subtitle: "knowing cue ball hit point effect"),
+        BilliardTips(image: "solid_two", title: "Hand Form", subtitle: "stabilize your cue stick using different hand form"),
+        BilliardTips(image: "solid_three", title: "Diamond System", subtitle: "learn how to aim more accurately using diamond system")
+    ]
+    
     var body: some View {
-//        NavigationStack{
-            ZStack{
-                Image("InfoBG")
+        ScrollView {
+            ZStack {
+                Image(.infoBG)
+                    .scaledToFit()
                 
-//                HStack{
-                    ScrollView {
-                        VStack(alignment: .center, spacing: 10) {
-                            Image("information1")
-                                .padding(.leading, 50)
-                            
-                            Image("information1")
-                                .padding(.leading, 50)
-                            
-                            Image("information1")
-                                .padding(.leading, 50)
+                LazyVStack(alignment:.center, spacing: 24) {
+                    ForEach(dataBilliardTips, id: \.self) { data in
+                        Button {
+                            print("item clicked: \(data)")
+                        } label: {
+                            HStack {
+                                Image("\(data.image)")
+                                    .resizable()
+                                    .frame(width: 250, height: 250)
+                                    .padding(.trailing, 24)
+                                VStack {
+                                    Text("\(data.title)")
+                                        .font(Font.custom("SFPro-ExpandedBold", size: 40.0))
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.white)
+                                    Text("\(data.subtitle)")
+                                        .font(.callout)
+                                        .foregroundStyle(.white)
+                                        .padding(.top, -20)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                            .frame(maxWidth: 700)
                         }
-                    }
-                    
-//                    VStack{
-//                        Text("Diamond System").font(Font.custom("SFPro-ExpandedBold", size: 40.0))
-//                            .font(.system(size: 50))
-//                            .fontWeight(.semibold)
-//                            .foregroundColor(.white)
-//                            .padding(.bottom, 5)
-//                        //                                            .tracking(2)
-//                        
-//                        Text("learn how to aim more accurately using diamond system")
-//                            .font(.body)
-//                            .foregroundColor(.white)
-//                    }
-//                }
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading){
-                        Button(action: {
-                            presentationMode.wrappedValue.dismiss()
-                        }){
-                            Image("back")
-                        }
-                        .padding(.top, 30)
                     }
                 }
             }
-//        }
+            .padding(.all, -60)
+        }
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .scrollIndicators(.hidden)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading){
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }){
+                    Image("back")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .padding(.top, 48)
+                .padding(.leading, -48)
+            }
+        }
+        .toolbarBackground(.hidden, for: .navigationBar)
         .navigationBarBackButtonHidden(true)
     }
 }
