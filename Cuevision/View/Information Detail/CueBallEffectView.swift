1f4407b96cdbf93c5561331ba61aed16cd210dfb
gabrielabernice <gabrielabernice03@gmail.com>
2024-07-23 16:34:13 +0700

    adding the back (X) button

--- Cuevision/View/Information Detail/CueBallEffectView.swift
+++ Cuevision/View/Information Detail/CueBallEffectView.swift
@@ -11,24 +11,42 @@ struct CueBallEffectView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        ZStack{
-            GeometryReader(content: { geometry in
-                Image("InfoBG")
-                Image("InfoBall1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height:geometry.size.height)
-            })
-            
-            VStack{
-                Text("Cue Ball Effect").font(Font.custom("SFPro-ExpandedBold", size: 40.0))
-                    .foregroundColor(.white)
-                Image("InfoCueBallEffect")
+        NavigationStack{
+            ZStack{
+                GeometryReader(content: { geometry in
+                    Image("InfoBG")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+                    Image("InfoBall1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height:geometry.size.height)
+                })
                 
+                VStack{
+                    Text("Cue Ball Effect").font(Font.custom("SFPro-ExpandedBold", size: 40.0))
+                        .foregroundColor(.white)
+                    Image("InfoCueBallEffect")
+                }
+                .padding(.leading, 200)
             }
-            .padding(.leading, 200)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image("BackCross")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                    }
+                    .padding(.top, 30)
+                }
+            }
+            .padding(.all, -20)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
--- Cuevision/View/Information Detail/DiamondSystemView.swift
+++ Cuevision/View/Information Detail/DiamondSystemView.swift
@@ -11,25 +11,44 @@ struct DiamondSystemView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        ZStack{
-            GeometryReader(content: { geometry in
-                Image("InfoBG")
-                Image("InfoBall3")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height:geometry.size.height)
-            })
-            
-            VStack{
-                Text("Diamond System").font(Font.custom("SFPro-ExpandedBold", size: 40.0))
-                    .foregroundColor(.white)
-                Spacer()
-                Image("InfoDiamondSystem")
+        NavigationStack{
+            ZStack{
+                GeometryReader(content: { geometry in
+                    Image("InfoBG")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+                    Image("InfoBall3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height:geometry.size.height)
+                })
+                
+                VStack{
+                    Text("Diamond System").font(Font.custom("SFPro-ExpandedBold", size: 40.0))
+                        .foregroundColor(.white)
+                    Spacer()
+                    Image("InfoDiamondSystem")
+                }
+                .padding(.leading, 320)
+                .padding(.vertical, 70)
             }
-            .padding(.vertical, 30)
-            .padding(.leading, 300)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image("BackCross")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                    }
+                    .padding(.top, 30)
+                }
+            }
+            .padding(.all, -20)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
